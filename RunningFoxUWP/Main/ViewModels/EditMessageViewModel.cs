b2eda7dfc19d55ef355adbfd89fac49137625750
OoopOoop <anastasiiabaranova@yahoo.co.uk>
2016-08-08 21:51:13 +0100

    add timepicker

--- RunningFoxUWP/Main/ViewModels/EditMessageViewModel.cs
+++ RunningFoxUWP/Main/ViewModels/EditMessageViewModel.cs
@@ -12,14 +12,7 @@ namespace Main.ViewModels
    public class EditMessageViewModel:ViewModelBase
     {
 
-        private ObservableCollection<object> _testCollection;
-        public ObservableCollection<object> TestCollection
-        {
-            get { return _testCollection; }
-            set { _testCollection = value; OnPropertyChanged(); }
-        }
-
-
+      
 
         private RelayCommand _saveCommand;
         public RelayCommand SaveCommand => _saveCommand ?? (_saveCommand = new RelayCommand(saveNewMessage));
@@ -36,24 +29,7 @@ namespace Main.ViewModels
             base._navigationService = navigationService;
 
 
-            TestCollection = new ObservableCollection<object>();
-            
-            TestCollection.Add("1");
-            TestCollection.Add("2");
-            TestCollection.Add("3");
-            TestCollection.Add("4");
-            TestCollection.Add("5");
-            TestCollection.Add("6");
-            TestCollection.Add("7");
-            TestCollection.Add("8");
-            TestCollection.Add("9");
-            TestCollection.Add("10");
-            TestCollection.Add("11");
-            TestCollection.Add("12");
-            TestCollection.Add("13");
-            TestCollection.Add("14");
-            TestCollection.Add("15");
-            TestCollection.Add("16");
+           
         }
     }
 }
--- RunningFoxUWP/Main/Views/EditMessagePage.xaml.cs
+++ RunningFoxUWP/Main/Views/EditMessagePage.xaml.cs
@@ -21,122 +21,20 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Main.Views
 {
+
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
     public sealed partial class EditMessagePage : Page
     {
-       // public List<object> numbersList { get; set; }
-
 
         public EditMessagePage()
         {
             this.InitializeComponent();
 
-
-            //numbersList = new List<object>();
-
-
-            //numbersList.Add("1");
-            //numbersList.Add("2");
-            //numbersList.Add("3");
-            //numbersList.Add("4");
-            //numbersList.Add("5");
-            //numbersList.Add("6");
-            //numbersList.Add("7");
-            //numbersList.Add("8");
-            //numbersList.Add("9");
-            //numbersList.Add("10");
-            //numbersList.Add("11");
-            //numbersList.Add("12");
-            //numbersList.Add("13");
-            //numbersList.Add("14");
-            //numbersList.Add("15");
-            //numbersList.Add("16");
-
-
-            //loadNumbers();
-
-
-
-            //ListView1.ItemsSource = NewNumbersList;
-
-            //ListView1.SelectedIndex = 0;
-
         }
 
-
-        //private ObservableCollection<object> _newNumbersList;
-        //public ObservableCollection<object> NewNumbersList => _newNumbersList ?? (_newNumbersList = new ObservableCollection<object>());
-
-        
-
-        //public IEnumerable<object> Get(int skip, int take)
-        //{
-        //    return numbersList.Skip(skip).Take(take);
-        //}
-
-
-
-        //private void loadNumbers()
-        //{
-        //    var numbers = Get(NewNumbersList.Count(), 5);
-        //    foreach (var item in numbers)
-        //    {
-        //        NewNumbersList.Add(item);
-        //    }
-        //}
-
-
-
-        //private void testBtn_Click(object sender, RoutedEventArgs e)
-        //{
-        //    ListView1.SelectedIndex = ListView1.SelectedIndex + 1;
-        //    ListView1.ScrollIntoView(ListView1.Items[ListView1.SelectedIndex]);
-
-        //    loadNumbers();
-
-
-
-        //    if (ListView1.SelectedIndex>=6)
-        //    {
-        //        NewNumbersList.Remove("1");
-        //        NewNumbersList.Remove("2");
-        //        NewNumbersList.Remove("3");
-        //        //NewNumbersList.Remove("4");
-        //        //NewNumbersList.Remove("5");
-
-        
-        //        NewNumbersList.Add("1");
-        //        NewNumbersList.Add("2");
-        //        NewNumbersList.Add("3");
-        //        //NewNumbersList.Add("4");
-        //        //NewNumbersList.Add("5");
-        //    }
-
-
-
-        //if (testLst.SelectedIndex > 0)
-        //{
-        //    testLst.ScrollIntoView(testLst.Items[testLst.SelectedIndex - 1]);
-        //}
-
-        //if (testLst.SelectedIndex < testLst.Items.Count-1)
-        //{
-        //    testLst.ScrollIntoView(testLst.Items[testLst.SelectedIndex - 1]);
-        //}
-    //}
-
-
-    //private void _addNumbers(int numbers)
-    //{
-
-    //}
-
-    //private void _removeNumbers(int numbers)
-    //{
-
-    //}
-
-  }
+    }
 }
