600e73381e7ab0712bf5a5911902f57f6aaf6227
Ana <Anastasiia.Baranova@galaleisure.com>
2017-05-19 17:16:29 +0100

    Add start stop running program.

--- RunningFoxUWP/Main/ViewModels/PlayPageViewModel.cs
+++ RunningFoxUWP/Main/ViewModels/PlayPageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.UI.Xaml;
 using GalaSoft.MvvmLight.Messaging;
 using GalaSoft.MvvmLight.Views;
@@ -10,47 +11,104 @@ namespace Main.ViewModels
     public class PlayPageViewModel: ViewModelBase
     {
         private DispatcherTimer _timer;
-        private int _basetime;
-        
+        private TimeSpan _basetime;
+
+
+        //private int currentMessageIndex=0;
+
         private MessageSetTable _playCollection;
         public  MessageSetTable PlayCollection
         {
             get { return _playCollection; }
             set { _playCollection = value; OnPropertyChanged(); }
         }
-        
-        private RelayCommand _startProgrammCommand;
-        public RelayCommand StartProgrammCommand => _startProgrammCommand ?? (_startProgrammCommand = new RelayCommand(StartProgram));
-        
-        private void StartProgram()
+
+
+        private RelayCommand _setNextProgrammCommand;
+        public RelayCommand SetNextProgrammCommand => _setNextProgrammCommand ?? (_setNextProgrammCommand = new RelayCommand(SetNextProgram, CanSetProgram));
+
+        private void SetNextProgram()
         {
-            _basetime = PlayCollection.MessageCollection[0].DisplayTime.Minutes;
-
-            TimeLeft = _basetime.ToString();
-
-            _timer = new DispatcherTimer();
-            _timer.Interval = new TimeSpan(0,0,0,1);
-            _timer.Tick += _timer_Tick;
             
-            _timer.Start();
         }
 
-        private void _timer_Tick(object sender, object e)
+        private bool CanSetProgram() => PlayCollection.MessageCollection.Count != 0;
+
+
+
+        private RelayCommand _pauseProgrammCommand;
+        public RelayCommand PauseProgrammCommand => _pauseProgrammCommand ?? (_pauseProgrammCommand = new RelayCommand(PauseProgram));
+
+        private void PauseProgram()
         {
-            _basetime = _basetime - 1;
-            TimeLeft = _basetime.ToString();
-            if (_basetime == 0)
+            if (_timer.IsEnabled)
             {
                 _timer.Stop();
             }
+            else
+            {
+                _timer.Start();
+            }
         }
 
+
+        private RelayCommand _startProgrammCommand;
+        public RelayCommand StartProgrammCommand => _startProgrammCommand ?? (_startProgrammCommand = new RelayCommand(StartProgram, CanStartProgram));
+        
+        private bool CanStartProgram() => !_timer.IsEnabled;
+
+
+        private void StartProgram()
+        {
+            //var time = PlayCollection.MessageCollection[currentMessageIndex].DisplayTime;
+
+            //TODO: check for null; create a playlist with previous message, and next one
+
+            var time = PlayCollection.MessageCollection[0].DisplayTime;
+
+            _basetime = new TimeSpan(0,time.Hours, time.Minutes, time.Seconds);
+
+            TimeLeft = _basetime.ToString();
+            _timer.Interval = new TimeSpan(0, 0, 1);
+
+            _timer.Tick += _timer_Tick;
+            _timer.Start();         
+
+            PlayCollection.MessageCollection.RemoveAt(0);
+        }
+
+        private void _timer_Tick(object sender, object e)
+        {  
+            _basetime = _basetime.Subtract(TimeSpan.FromSeconds(1));
+            TimeLeft = _basetime.ToString();
+            if (_basetime == new TimeSpan(0, 0, 0, 0))
+            {
+                _timer.Stop();
+                //  currentMessageIndex++;
+                //setNewMessage(currentMessageIndex);
+
+                setNewMessage();
+            }
+        }
+
+        //private void setNewMessage(int index)
+        //{
+        //    CurrentMessage = PlayCollection?.MessageCollection[index].MessageText;
+        //    StartProgram();
+        //}
+
+
+        private void setNewMessage()
+        {
+            CurrentMessage = PlayCollection?.MessageCollection[0].MessageText;
+            StartProgram();
+        }
 
         public PlayPageViewModel(INavigationService navigationService)
         {
             base._navigationService=navigationService;
             getProgramToPlay();
-            
+            _timer = new DispatcherTimer();
         }
 
         private string _currentMessage;
