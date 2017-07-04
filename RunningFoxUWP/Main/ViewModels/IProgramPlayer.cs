511745c5a197c0d4282f1e91fc67bf55c4a3f215
OoopOoop <OoopOoop@users.noreply.github.com>
2017-07-04 23:08:17 +0100

    add properties to the ProgramPlayer

--- RunningFoxUWP/Main/ViewModels/IProgramPlayer.cs
+++ RunningFoxUWP/Main/ViewModels/IProgramPlayer.cs
@@ -1,12 +1,21 @@
 ﻿using Main.Models;
+using System;
 using System.Collections.Generic;
 
 namespace Main.ViewModels
 {
     public interface IProgramPlayer
     {
-        List<MessageTable> PlayCollection { get; set; }
-        void PlayProgram();
+        TimeSpan CurrentMessageTime { get; set; }
+        TimeSpan PreviousMessageTime { get; set; }
+        TimeSpan NextMessageTime { get; set; }
+        TimeSpan TotalExerciseTime { get; set; }
+        string CurrentMessage { get; set; }
+        string PreviousMessage { get; set; }
+        string NextMessage { get; set; }
+        string PlayButtonContext { get; set; }
+        void SetProgram(List<MessageSetTable> PlayCollection);
+        void PlayProgram(MessageTable messageToPlay);
         void StopProgram();
         string DisplayTime();
     }
--- RunningFoxUWP/Main/ViewModels/ProgramPlayer.cs
+++ RunningFoxUWP/Main/ViewModels/ProgramPlayer.cs
@@ -1,22 +1,116 @@
 ﻿using Main.Models;
+using System;
 using System.Collections.Generic;
 using Windows.UI.Xaml;
 
 namespace Main.ViewModels
 {
-    public class ProgramPlayer : IProgramPlayer
+    public class ProgramPlayer : ViewModelBase, IProgramPlayer
     {
         private DispatcherTimer _timer;
-        public List<MessageTable> PlayCollection { get; set; }
+        private int _currentPlayingMessageIndex;
 
-        public ProgramPlayer(List<MessageTable> collection)
+        private List<MessageSetTable> PlayCollection { get; set; }
+
+        private TimeSpan _currentMessageTime;
+        public TimeSpan CurrentMessageTime
         {
-            PlayCollection = collection;
+            get { return _currentMessageTime; }
+            set { _currentMessageTime = value; OnPropertyChanged(); }
         }
 
-        public void PlayProgram()
+        private TimeSpan _previousMessageTime;
+        public TimeSpan PreviousMessageTime
         {
+            get { return _previousMessageTime; }
+            set { _previousMessageTime = value; OnPropertyChanged(); }
+        }
+
+        private TimeSpan _nextMessageTime;
+        public TimeSpan NextMessageTime
+        {
+            get { return _nextMessageTime; }
+            set { _nextMessageTime = value; OnPropertyChanged(); }
+        }
+
+        private TimeSpan _totalExerciseTime;
+        public TimeSpan TotalExerciseTime
+        {
+            get { return _totalExerciseTime; }
+            set { _totalExerciseTime = value; OnPropertyChanged(); }
+        }
+
+        private string _currentMessage;
+        public string CurrentMessage
+        {
+            get { return _currentMessage; }
+            set { _currentMessage = value; OnPropertyChanged(); }
+        }
+        
+        private string _previousMessage;
+        public string PreviousMessage
+        {
+            get { return _previousMessage; }
+            set { _previousMessage = value; OnPropertyChanged(); }
+        }
+        
+        private string _nextMessage;
+        public string NextMessage
+        {
+            get { return _nextMessage; }
+            set { _nextMessage = value; OnPropertyChanged(); }
+        }
+
+        private string _playButtonContext;
+        public string PlayButtonContext
+        {
+            get { return _playButtonContext; }
+            set { _playButtonContext = value; OnPropertyChanged(); }
+        }
+
+        public  bool IsMessagePlaying()
+        {
+            return _timer.IsEnabled;
+        }
+        
+        public void SetProgram(List<MessageSetTable> _playCollection)
+        {
+            PlayCollection = _playCollection;
+            _currentPlayingMessageIndex = 0;
+            SetMessageAndTime(_currentPlayingMessageIndex);
+        }
+
+    
+        public void PlayProgram(MessageTable messageToPlay)
+        {
+            if(IsMessagePlaying())
+            {
+                StopProgram();
+                return;
+            }
+
+            CurrentMessageTime = messageToPlay.DisplayTime;
+            _timer.Interval = new TimeSpan(0, 0, 1);
+            _timer.Tick += _timer_Tick;
             _timer.Start();
+        }
+        
+        private void _timer_Tick(object sender, object e)
+        {
+            TotalExerciseTime = TotalExerciseTime.Add(TimeSpan.FromSeconds(1));
+            if(CurrentMessageTime!=TimeSpan.FromSeconds(0))
+            {
+                CurrentMessageTime = CurrentMessageTime.Subtract(TimeSpan.FromSeconds(1));
+                return;
+            }
+
+            _timer.Stop();
+            SetMessageAndTime(_currentPlayingMessageIndex);
+        }
+        
+        private void SetMessageAndTime(int currentIndex)
+        {
+
         }
 
         public void StopProgram()
